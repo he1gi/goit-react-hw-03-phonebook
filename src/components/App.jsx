fd088b0470ae645b0cf4aa042e19ee376d51c05e
he1gi <oleh.mishchenko1@gmail.com>
2022-09-24 13:26:50 +0300

    added final task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,18 +9,12 @@ import { MAIN_WRAPPER } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     name: '',
     filter: '',
   };
 
   formSubmitHandler = data => {
-    console.log(data);
     const { contacts } = this.state;
     if (
       contacts.find(
@@ -50,6 +44,20 @@ export class App extends Component {
       return { contacts: contacts.filter(({ id }) => deleteId !== id) };
     });
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contscts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter, contacts } = this.state;
